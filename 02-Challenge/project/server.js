ad7bb5bed731618a74e97667b78130ef3f9647ce
Ra <ranilsd86@gmail.com>
2023-11-06 18:39:57 -0500

    completed server.js

--- 02-Challenge/project/server.js
+++ 02-Challenge/project/server.js
@@ -1,17 +1,49 @@
+//importing dependencies
+
+const path = require('path');
 const express = require('express');
-const routes = require('./controllers'); // make sure this path is correct
-const sequelize = require('./config/connection'); // make sure this path is correct
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+//sequelize setup
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//express server
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const hbs = exphbs.create({helpers});
 
-app.use(routes);
+    //creating a session object
+    const sess = {
+        secret: process.env.SECRET,
+        cookie: {},
+        resave: false,
+        saveUninitialized: true,
+        store: new SequelizeStore({
+            db: sequelize
+        }),
+        //setting session to expire after 15 min
+        expires: new Date(Date.now() + (15*60000))
 
-// Sync the database and force it to recreate tables based on the current model definitions
-sequelize.sync({ force: true }).then(() => { // Only use { force: true } if you're sure you want to drop tables
-  console.log('Database & tables created!');
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
+    };
+
+    //template engine to use
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+
+    //middleware
+    app.use(session(sess));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(routes);
+
+    sequelize.sync({ force: false }).then(() => {
+        app.listen(PORT, () => console.log(`now listning on port ${PORT}`));
+    });
